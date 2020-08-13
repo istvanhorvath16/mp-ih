39a5595b11cb1eb26d81a7b42fd2bacc3a8f3c52
Istvan Horvath <istvanhorvath0116@gmail.com>
2020-08-13 18:36:14 +0300

    Updates

---
+++ precache-manifest.32f5da8fa99e0e577ccb49babc168337.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f073c11b1e06b12176a605028f8f5bf",
+    "url": "/mp-ih/index.html"
+  },
+  {
+    "revision": "1aeb549baf63f8d339bb",
+    "url": "/mp-ih/static/css/2.dc318d56.chunk.css"
+  },
+  {
+    "revision": "32afffe42970150a35ec",
+    "url": "/mp-ih/static/css/3.2f6b9353.chunk.css"
+  },
+  {
+    "revision": "585ff89a0dcc095881e5",
+    "url": "/mp-ih/static/css/main.c615436a.chunk.css"
+  },
+  {
+    "revision": "1aeb549baf63f8d339bb",
+    "url": "/mp-ih/static/js/2.e77839b3.chunk.js"
+  },
+  {
+    "revision": "32afffe42970150a35ec",
+    "url": "/mp-ih/static/js/3.43b1c08c.chunk.js"
+  },
+  {
+    "revision": "f56b36355098ac5f6653",
+    "url": "/mp-ih/static/js/4.e2994b92.chunk.js"
+  },
+  {
+    "revision": "585ff89a0dcc095881e5",
+    "url": "/mp-ih/static/js/main.65d3b6f8.chunk.js"
+  },
+  {
+    "revision": "45021e8859c23b4dabab",
+    "url": "/mp-ih/static/js/runtime-main.60d50f9f.js"
+  },
+  {
+    "revision": "a8ff96e1fe989b91a5fb0879c4ad2527",
+    "url": "/mp-ih/static/media/avatar.a8ff96e1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mp-ih/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mp-ih/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mp-ih/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mp-ih/static/media/slick.f97e3bbf.svg"
+  }
+]);
